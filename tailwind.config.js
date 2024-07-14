115da0aa652db2446a65511c3d95bdf16cce999d
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-14 12:14:55 +0200

    add colors and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGrey: "#2a4144",
+        mediumGrey: "#86a2a5",
+        green: "#0c7d69",
+        lightGreen: "#e0f1e8",
+        redError: "#d94747",
+      },
+    },
   },
   plugins: [],
 };
-
